7ddecb94a87d23df1890fde0ebf678020a76b9e2
madelinedulchin <madeline.dulchin@gmail.com>
2023-03-01 16:15:25 -0500

    no home button on see more photos

--- client/src/EmbroideryCard.js
+++ client/src/EmbroideryCard.js
@@ -1,6 +1,6 @@
 import { useState} from 'react';
 import Lightbox from "yet-another-react-lightbox"
-function EmbroideryCard({eProjects}){
+function EmbroideryCard({eProjects, setIsClicked, isClicked}){
     const [open, setOpen] = useState(false);
     const [more, setMore] = useState([]);
 const proj = eProjects.map((eProject) => {
@@ -8,17 +8,20 @@ const proj = eProjects.map((eProject) => {
     return (
     <div className="emCard" >
         <img src={eProject.cover} alt={eProject.title} />
-        <p className='desc'>{eProject.description}</p>
-        <span className='info'>
-        <h2>{eProject.title}</h2>
+        <p className='desc'>{eProject.description}
         <button className="moreButton" type="button" onClick={() => morePhotos(eProject.photos)}>See more photos</button>
-        </span>
+        </p>
+        {/* <span className='info'> */}
+        <h2>{eProject.title}</h2>
+        {/* <button className="moreButton" type="button" onClick={() => morePhotos(eProject.photos)}>See more photos</button> */}
+        {/* </span> */}
     </div>
     )
 })
 
 function morePhotos(e){
     setOpen(!open)
+    setIsClicked(!isClicked)
     setMore(
         e.map((ph) => {
       return  (
@@ -27,13 +30,17 @@ function morePhotos(e){
     })
     )
 }
+function lessPhotos(){
+    setOpen(!open)
+    setIsClicked(!isClicked)
+}
 
     return (
         <div className="emCardContainer">
          {open ? 
          <div className='moreContainer'>
         {more}
-         <button className="lessButton" type="button" onClick={() => setOpen(!open)}>Close</button>
+         <button className="lessButton" type="button" onClick={() => lessPhotos()}>Close</button>
         </div>
          :   
          proj

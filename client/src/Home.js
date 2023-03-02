b1552465f8039fbaf3c5fd7765721524a05c7585
madelinedulchin <madeline.dulchin@gmail.com>
2023-03-02 14:38:19 -0500

    css here css there home mostly

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,13 +4,10 @@ function Home(){
 
     return (
         <div className="Home">
-            <span className="firstName">
             <h1 className="first">Madeline</h1>
-            </span>
+            <span className='homeColor'></span>
             <p className="EAdescription">Embroidery Artist</p>
-            <span className="lastName">
             <h1 className="last">Dulchin</h1>
-            </span>
             <p className="SWdescription">Software Engineer</p>
             <Link to="/embroidery-projects" className='EP'>➤</Link>
             <Link to="/software-projects" className='SP'>➤</Link>
